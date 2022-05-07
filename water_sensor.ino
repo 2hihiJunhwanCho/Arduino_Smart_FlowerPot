e7d51da1b3a3ff53453def2ac7520900ae9e4f28
Bryan Hwan Cho <61900293+BryanHwanCho@users.noreply.github.com>
2022-05-07 16:29:57 +0900

    Add moistures

--- water_sensor.ino
+++ water_sensor.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+String str;
+int oldmos=-1;
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 int Sensor_pin = A1; // Sensor pin = A1
@@ -19,21 +20,28 @@ void setup()
 
 void loop()
 {
+  int soil_mos;
+  
+  soil_mos=1023-analogRead(0); 
+  soil_mos=map(soil_mos, 0, 1023, 0, 100);
+  if (oldmos!=soil_mos) {
+    lcd.setCursor(0,0);
+    lcd.print("Soil Moist.:");
+    lcd.print(soil_mos);
+    lcd.print("%");
+  }
+  
   Serial.println(analogRead(Sensor_pin));
   delay(100); // s = 0.1
 
   if(analogRead(Sensor_pin) > 400){
     lcd.clear();
     digitalWrite(LED_R,HIGH);
-    digitalWrite(LED_G,LOW);
-    lcd.setCursor(0,0);
-    lcd.print("Lack of Water");
+    digitalWrite(LED_G,LOW);    
   }
   else{
     lcd.clear();
     digitalWrite(LED_R,LOW);
     digitalWrite(LED_G,HIGH);
-    lcd.setCursor(0,1);
-    lcd.print("Enough");
   }
 }
